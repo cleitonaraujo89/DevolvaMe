c844da9d2903bc2cf954c67ca220c5dd5a15ca9a
BrunoCesarCRJ <101814731+BrunoCesarCRJ@users.noreply.github.com>
2024-06-15 19:02:22 -0300

    Alterações 15/06/24
    - Nova Homepage
    - Homepage antiga transformada propriamente em uma página informativa
    - Footer adicional na tela de Cadastro
    - Leves mudanças estéticas referentes aos títulos de outras páginas (a serem avaliados pelo grupo)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/autenticador/auth.service';
+import { AlertController } from '@ionic/angular'
+import 'firebase/compat/auth';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private authService: AuthService, private router: Router, private alertController: AlertController ) {}
 
+  email: string='';
+  password: string='';
+  
+  async home() {
+    try {
+      await this.authService.signInWithEmail(this.email, this.password);
+      this.router.navigate(['/user']);
+    } catch (error) {
+      /* console.log("falou", error) */
+      const alert = await this.alertController.create({
+        header: 'Erro de Login',
+        message: 'Usuário ou senha incorretos.',
+        buttons: ['OK']
+      });
+      await alert.present() ;
+    }
+  }
+
+  async loginWithGoogle() {
+    try {
+      await this.authService.signInWithGoogle();
+      this.router.navigate(['/user']);
+    } catch (error) {
+      console.log("falou", error)
+/*       const alert = await this.alertController.create({
+        header: 'Erro de Login',
+        message: 'Usuário ou senha incorretos.',
+        buttons: ['OK']
+      });
+      await alert.present(); */
+    }
+  }
 }
